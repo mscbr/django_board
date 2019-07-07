0b4e9fdd1b42e6f43d96add50951426bc675ed84
mscbr <scibor.maciej@gmail.com>
2019-07-07 13:29:54 +0200

    environment related file changes

--- src/django_board/views.py
+++ src/django_board/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse("<h1>hello django</h1>")
+    return render(request, "hello.html")
 
 
 def about_page(request):
