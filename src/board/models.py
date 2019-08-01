8c3a118e4d50538bbca3af02c3a9f8c3a6bc9a6a
mscbr <scibor.maciej@gmail.com>
2019-08-01 13:48:52 +0200

    displaying search results

--- src/board/models.py
+++ src/board/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 from django.utils import timezone
 
 # Create your models here.
@@ -10,10 +11,24 @@ class BlogPostQueryset(models.QuerySet):
     def published(self):
         now = timezone.now()
         return self.filter(publish_date__lte=now)
+    def search(self, query):
+        lookup = (
+                    Q(title__icontains=query) | 
+                    Q(content__icontains=query) |
+                    Q(slug__icontains=query)
+                )
+
+        return self.filter(lookup)
 
 class BoardPostManager(models.Manager):
     def get_queryset(self):
          return BlogPostQueryset(self.model, using=self._db)
+    def published(self):
+        return self.get_queryset().published()
+    def search(self, query=None):
+        if query is None:
+            return self.get_queryset().none()
+        return self.get_queryset().published().search(query)
 
 class BoardPost(models.Model):
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
